b668e590108e4a3e5a6605395fe74c8912db5a32
Vasyl Demianov <vasyl.demianov@globallogic.com>
2023-01-05 13:00:16 +0200

    Fixed paths to new workdir.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
-export PATH=$PATH:/style_check
-export PYTHONPATH=/style_check
+export PATH=$PATH:/style_police
+export PYTHONPATH=/style_police
 
 cd /github/workspace
-python3 /style_check/check_commit_style.py ${1}
+python3 /style_police/check_commit_style.py ${1}
